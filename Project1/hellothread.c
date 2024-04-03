be0195bd090c16f686ba7bccd3d76bb5f9069c58
Xander Bailey <xander.bailey@gmail.com>
2024-04-03 11:19:19 -0700

    Rename print_id() to print_message()

--- Project1/hellothread.c
+++ Project1/hellothread.c
@@ -2,12 +2,12 @@
 #include <stdio.h>
 
 
-void *print_id(void *arg)
+void *print_message(void *arg)
 {
-    char* thread_name = arg;
+    char* message = arg;
     for (int i = 1; i <= 4; i++)
     {
-        printf("%s: %d\n", thread_name, i);
+        printf("%s: %d\n", message, i);
     }
 
     return NULL;
@@ -21,10 +21,10 @@ int main(int argc, char const *argv[])
     puts("Launching threads");
 
     pthread_t thread_1;
-    pthread_create(&thread_1, NULL, print_id, "thread 1");
+    pthread_create(&thread_1, NULL, print_message, "thread 1");
 
     pthread_t thread_2;
-    pthread_create(&thread_2, NULL, print_id, "thread 2");
+    pthread_create(&thread_2, NULL, print_message, "thread 2");
 
     pthread_join(thread_1, NULL);
     pthread_join(thread_2, NULL);
