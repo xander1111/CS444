23b88e8f2b5f6378b5f6737079c36e6ef17e43dc
Xander Bailey <xander.bailey@gmail.com>
2024-04-07 19:47:18 -0700

    Add error checking to hellothread.c

--- project1/hellothread.c
+++ project1/hellothread.c
@@ -21,8 +21,19 @@ int main(int argc, char const *argv[])
 
     pthread_t thread_1, thread_2;
 
-    pthread_create(&thread_1, NULL, print_message, "thread 1");
-    pthread_create(&thread_2, NULL, print_message, "thread 2");
+    int rc =  pthread_create(&thread_1, NULL, print_message, "thread 1");
+    if (rc != 0)
+    {
+        printf("An error while creating a thread\n");
+        return 1;
+    }
+
+    rc = pthread_create(&thread_2, NULL, print_message, "thread 2");
+    if (rc != 0)
+    {
+        printf("An error while creating a thread\n");
+        return 1;
+    }
 
     pthread_join(thread_1, NULL);
     pthread_join(thread_2, NULL);
