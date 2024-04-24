b4d52ea088db017005e1fd9c8980e297f19e157b
Xander Bailey <xander.bailey@gmail.com>
2024-04-23 20:23:57 -0700

    Add semaphore helper function

--- project3/pc.c
+++ project3/pc.c
@@ -12,6 +12,28 @@ int event_create_count;
 int max_events;
 
 struct eventbuf *buffer;
+sem_t *buffer_lock;
+
+sem_t *prod_sem;
+sem_t *cons_sem;
+
+
+sem_t *sem_open_temp(const char *name, int value)
+{
+    sem_t *sem;
+
+    // Create the semaphore
+    if ((sem = sem_open(name, O_CREAT, 0600, value)) == SEM_FAILED)
+        return SEM_FAILED;
+
+    // Unlink it so it will go away after this process exits
+    if (sem_unlink(name) == -1) {
+        sem_close(sem);
+        return SEM_FAILED;
+    }
+
+    return sem;
+}
 
 
 void *producer(void *arg) {
