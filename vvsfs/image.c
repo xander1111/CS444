034df454b73574c43ed991c3760e94a23672f756
Xander Bailey <xander.bailey@gmail.com>
2024-05-05 15:00:00 -0700

    Implement image.c

---
+++ vvsfs/image.c
@@ -0,0 +1,19 @@
+#include "image.h"
+
+#include <fcntl.h>
+#include <unistd.h>
+
+int image_open(char *filename, int truncate)
+{
+    if (truncate)
+        image_fd = open(filename, O_RDWR|O_CREAT|O_TRUNC, 0600);
+    else
+        image_fd = open(filename, O_RDWR|O_CREAT, 0600);
+
+    return image_fd;
+}
+
+int image_close(void)
+{
+    return close(image_fd);     
+}
