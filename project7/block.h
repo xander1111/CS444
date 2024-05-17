45413151d32713732dd92ab6796015331182a3e7
Xander Bailey <xander.bailey@gmail.com>
2024-05-17 14:19:30 -0700

    Update constant names

--- project7/block.h
+++ project7/block.h
@@ -5,10 +5,8 @@
 #define SUPERBLOCK 0
 #define INODE_FREE_BLOCK 1
 #define DATA_FREE_BLOCK 2
-#define INODE_DATA_0 3
-#define INODE_DATA_1 4
-#define INODE_DATA_2 5
-#define INODE_DATA_3 6
+#define INODE_DATA_BLOCK_START 3
+#define INODE_DATA_BLOCK_COUNT 4
 
 unsigned char *bread(int block_num, unsigned char *block);
 void bwrite(int block_num, unsigned char *block);
